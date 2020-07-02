03d055ec8d995b8fd41a94d28902e1fe2f27270b
chiazo <chiazo@live.unc.edu>
2020-07-01 23:50:18 -0700

    setup react

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3001;
 const userModel = require('../models/user');
-// const cors = require('cors');
+const cors = require('cors');
+const { response } = require('express');
 
-// app.use(cors({ origin: true }));
-
+app.use(cors({ origin: true }));
 app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.method, req.url);
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -30,6 +31,28 @@ app.get('/', (req, res) => {
     })
     .catch((e) => {
       res.status(500).send(e);
+    });
+});
+
+app.post('/users', (req, res) => {
+  userModel
+    .createUser(req.body)
+    .then((response) => {
+      res.status(201).send(response);
+    })
+    .catch((e) => {
+      res.status(500).send(e);
+    });
+});
+
+app.delete('/users/:id', (req, res) => {
+  userModel
+    .deleteUser(req.params.id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((e) => {
+      res.status(500).send(response);
     });
 });
 
